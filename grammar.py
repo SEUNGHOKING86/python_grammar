af7979bbd850ccd460d25f86443e5dca865d4e51
manager <admin@mindmap.ai>
2022-08-15 08:34:43 +0900

    08-15

--- grammar.py
+++ grammar.py
@@ -57,6 +57,7 @@ s.ljust(n) # 좌측 정렬
 s.center(n) # 가운데 정렬
 s.rjust(n) # 우측 정렬
 
+import re
 import string 
 
 num = int(input().strip())
@@ -91,3 +92,58 @@ animals = ['cat', 'dog', 'lion']
 sounds = ['meow', 'woof', 'roar']
 answer = dict(zip(animals, sounds))
 print(answer)
+
+
+# 2022-08-15
+# *solution 함수가 mylist의 i번째 원소와 i+1번째 원소의 차를 담은 일차원 리스트에 차례로 담아 리턴하도록 코드를 작성
+
+#1 번째 방법
+def solution(mylist):
+    answer = []
+    for i in range(len(mylist)-1):
+        answer.append(abs(mylist[i] - mylist[i+1]))
+        return answer
+
+#2 번쨰 방법 zip한수를 사용
+def solution(mylist):
+    answer = []
+    for number1, number2 in zip(mylist, mylist[1:]):
+        answer.append(abs(number1 - number2))
+        return answer
+
+mylist = [83, 48, 13, 4, 71, 11]
+print(solution(mylist))
+
+# *문자열 리스트 mylist를 입력받아, 이 리스트를 정수형 리스트로 바꾼 값을 리턴하는 함수
+
+def solution(mylist):
+    
+    list2 = list(map(int, mylist))
+    return list2
+mylist = ['1', '100', '33']
+print(solution(mylist))
+                
+
+# * mylist가 ['1', '100', '33'] 인 경우, solution 함수는 '110033'을 리턴
+
+# 1번재 방법
+def solution(mylist):
+    answer = ''
+    for i in mylist:
+        answer += i
+    return answer
+
+# 2번째 방법
+def solution(mylist):
+    return ''.join(mylist)
+
+
+# *'abc', 'abcabc', 'abcabcabc', 'abcabcabcabc ...' 과 같이 'abc'가 n번 반복되는 문자열 만들기
+
+n = 'abc'
+answer = 'abc' * n
+
+# 입력값에 맞는 별 찍기
+n = int(input().strip())
+for i in range(n + 1):
+    print('*' * i)
