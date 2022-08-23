faf551f4262e1eb7ff693694813e0e121aac2eae
seungho-yang <106039490+seungho-yang@users.noreply.github.com>
2022-08-23 09:34:53 +0900

    Create grammar2.py

---
+++ grammar2.py
@@ -0,0 +1,15 @@
+
+# 기존 코드 방식
+count = fresh_fruit.get('사화', 0)
+if count >= 4:
+	make_cider(count)
+else:
+	out_of_stock()
+	
+
+# 파이썬 스타일 코드 방식
+if (count := fresh_fruit.get('사과', 0)) >= 4:
+	make_cider(count)
+else:
+	out_of_stock()
+	
