e4b7ea4d8fcd9561cb6dc080560654627c939de1
manager <admin@mindmap.ai>
2022-08-29 06:59:13 +0900

    22.08.29

--- datastructures.py
+++ datastructures.py
@@ -1,94 +1,76 @@
-from imghdr import what
-from re import X
-from tempfile import tempdir
-from tkinter import N
 
+# -- 자료구조와 알고리즘 --  data structures
 
--- 자료구조와 알고리즘 --  data structures
+# 알고리즘(algorithm) = 어떤 문제를 해결하기 위한 절차, 방법, 명령어들의 집합(해결하고자 하는 문제에 따라 최적의 해법은 서로 다르다)
 
-알고리즘(algorithm) = 어떤 문제를 해결하기 위한 절차, 방법, 명령어들의 집합(해결하고자 하는 문제에 따라 최적의 해법은 서로 다르다)
-
-문자열(str) "This is a string"
-리스트(list) [5, 9, 2, 7]
-사전(dict) {'a': 6, 'b':4 }
-순서쌍(tuple)
-집합 (zip)
+# 문자열(str) "This is a string"
+# 리스트(list) [5, 9, 2, 7]
+# 사전(dict) {'a': 6, 'b':4 }
+# 순서쌍(tuple)
+# 집합 (zip)
 
 
 
-100개의 숫자 중
-549를 찾아라.
+# 100개의 숫자 중 549를 찾아라.
 
-입력으로 주어지는 리스트 x의 첫 원소와 마지막 원소의 합을 리턴하는 합
+# 입력으로 주어지는 리스트 x의 첫 원소와 마지막 원소의 합을 리턴하는 합
 
-answer = x[0] + x.pop()
-return answer
+def solution(x):
+    answer = x[0] + x.pop()
+    return answer
 
 
-선형 배열 (Linear Arrays)
+# 선형 배열 (Linear Arrays)
 
-선형 배열 (Arrays) - 원소들을 순서대로 늘어놓은 것
-리스트 (list)
-
-데이터를 늘어놓은 모양새를 말할 때는 배열 (array), Python 의 데이터형을 가리킬 때에는 리스트 (list) 라는 용어를 사용
+# 선형 배열 (Arrays) - 원소들을 순서대로 늘어놓은 것
+# 리스트 (list)
+# 데이터를 늘어놓은 모양새를 말할 때는 배열 (array), Python 의 데이터형을 가리킬 때에는 리스트 (list) 라는 용어를 사용
   
-리스트 (list)
+#리스트 (list)
 L = ['bob', 'cat', 'spam', 'progrmmers']
-
 L.POP() == 'progrmmers' 
-
 L = ['bob', 'cat', 'spam']
 L.append('banana')
 L = ['bob', 'cat', 'spam', 'banana']
-L.index('spam') == spam 을 바로 찾음
-
-
-
-
-
+#L.index('spam') == spam 을 바로 찾음
  
-리스트의 길이에 비례(선형시간)
-O(n)
-o(1)상수시간
-리스트의 길이와 상관없음
+#리스트의 길이에 비례(선형시간) O(n)
+#o(1)상수 시간 리스트의 길이와 상관없음
+
+#원소 맨끝 덧붙이기 .append() 맨 마지막에 덫 붙여짐
+#원소 맨끝 하나를 꺼내기 .pop() 맨 나지막 원소를 하나 빼냄
 
 
+#ㅁ 원소 삽입하기 insert() 원소 삭제하기 del()
+# 이런 연산들은 리스트의 길이가 길면 길수록 처리가 오래 걸리게 됨. 
+# 구체적으로 말하면 리스트의 길이에 실행 시간이 비례. 
+#리스트 길이가 100 배가 되면, 위 연산들을 실행하는 데 걸리는 시간도 100 배 커짐
 
-원소 맨끝 덧붙이기 .append() 맨 마지막에 덫 붙여짐
-원소 맨끝 하나를 꺼내기 .pop() 맨 나지막 원소를 하나 빼냄
-
-
-
-ㅁ 원소 삽입하기 insert() 원소 삭제하기 del()
-이런 연산들은 리스트의 길이가 길면 길수록 처리가 오래 걸리게 됨. 
-구체적으로 말하면 리스트의 길이예 실행 시간이 비례. 
-리스트 길이가 100 배가 되면, 위 연산들을 실행하는 데 걸리는 시간도 100 배 커짐
-
-원소 탐색하기 .index()
+#원소 탐색하기 .index()
 L.POP(INDEX)
 
+#3 번째 index에 65를 넣어라
 L.insert(3, 65)
 
-3번째 원소를 삭제하라.
+#3번째 원소를 삭제하라.
 del(L[2])
 
 
- L = [20, 37, 65, 72, 91]
- L.POP(2)
- ==>65
- L = [20, 37, 72, 91]
+L = [20, 37, 65, 72, 91]
+L.POP(2)==>65
+L = [20, 37, 72, 91]
  
- ㅁ 원소 탐색하기
+ #ㅁ 원소 탐색하기
  
  L = [20, 37, 72, 91]
  L.index(37)
  ==>1
  
  
- 1 문제
- 주어진 원소를 삽입할 위치를 찾는다.
- 해당 위치에 원소를 삽입한다.
- insert()
+#  1 문제
+#  주어진 원소를 삽입할 위치를 찾는다.
+#  해당 위치에 원소를 삽입한다.
+#  insert()
  
 def solution(L, x):
     index = len(L)
@@ -98,22 +80,18 @@ def solution(L, x):
             break
     L.insert(index, x)
     return L
-
 def solution(L, x):
     L.append(x)
     L.sort()
     return L	
 
+# 로직 다시 확인 해야 함
 A = [i for i in range(len(L)) if x-L[i]>0]	
 
- 2 문제 
- index() 매서드를 이용
- 리스트 슬라이싱을 이용해서 반복해서 찾는다.
+#  2 문제 
+#  index() 매서드를 이용
+#  리스트 슬라이싱을 이용해서 반복해서 찾는다.
  
- 
-
-
-
 def solution(L, x):
     answer = [ idx for idx, value in enumerate(L) if value == x ]
     return answer if answer else [-1]
